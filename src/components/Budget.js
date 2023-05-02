eab3ab0caf7294962dcc7de2a3d5b22b89f7335d
Serban Bondor <serbanbondor@yahoo.com>
2023-05-02 11:23:24 +0300

    task 2 done, budget as input with numbers and alerts

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,35 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, dispatch } = useContext(AppContext);
+
+  const handleBugdet = (value) => {
+    dispatch({
+      type: "SET_BUDGET",
+      payload: value,
+    });
+  };
+
+  const handleNumbersOnlyInput = (event) => {
+    if (!/[0-9]/.test(event.key) && event.key !== "Backspace") {
+      event.preventDefault();
+    }
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: £</span>
+      <input
+        required="required"
+        type="number"
+        step="10"
+        id="budget"
+        value={budget}
+        style={{ marginLeft: "2rem", size: 10 }}
+        onChange={(event) => handleBugdet(event.target.value)}
+        onKeyDown={handleNumbersOnlyInput}
+      ></input>
+    </div>
+  );
 };
 export default Budget;
